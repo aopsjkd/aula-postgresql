e6ea4124a7b8e871234b78cb6513626a033f38a8
willian <gordinhobololomtts@gmail.com>
2025-09-30 10:17:57 -0300

    funcao de conexao com banco

---
+++ db.py
@@ -0,0 +1,24 @@
+#pip install psycopg2
+#pip install dotenv
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+# Carregar variáveis do .env
+load_dotenv()
+
+params = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT")
+}
+def conectar():
+    try:
+        conexao = pg.connect(**params)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    except Exception as erro:
+        print(f"erro de conexao {erro}")
+        return None, None
